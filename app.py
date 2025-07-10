fa474cbb681f6e3ef5063d7c7b692c61173ffd63
NNHOLDING <nholdingsolutions@gmail.com>
2025-07-10 17:26:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ with st.container():
     st.markdown('<div class="form-container">', unsafe_allow_html=True)
 
     with st.form("formulario_alisto"):
-        st.subheader("📝 Almacèn Unimar")
+        st.subheader("📝 Almacen Unimar")
 
         col1, col2 = st.columns(2)
         with col1:
